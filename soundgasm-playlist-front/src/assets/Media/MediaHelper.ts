32d30b683e0fc43cada3ce93642eeca0c605621c
Ruan <ruan.oliveiralopes@hotmail.com>
2023-12-19 09:28:52 -0300

    new asset
    - Small play icon for the play now button in the audio component.

--- soundgasm-playlist-front/src/assets/Media/MediaHelper.ts
+++ soundgasm-playlist-front/src/assets/Media/MediaHelper.ts
@@ -5,4 +5,5 @@ import Next from "./Skip_Forward.png";
 import Up from "./Volume_Plus.png";
 import Down from "./Volume_Minus.png";
 import Trash from "./Trash.png";
-export { Pause, Play, Previous, Next, Up, Down, Trash };
+import Play_24 from "./Play_24.png";
+export { Pause, Play, Previous, Next, Up, Down, Trash, Play_24 };
