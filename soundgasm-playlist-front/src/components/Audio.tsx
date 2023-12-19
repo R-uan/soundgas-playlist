3e098dc6d6ffe0ff0c43c7b4be1193154e5a3e3b
Ruan <ruan.oliveiralopes@hotmail.com>
2023-12-19 07:11:30 -0300

    added delete icon
    - new deleted icon added into assets
    - implemented icon in the delete audio button

--- soundgasm-playlist-front/src/components/Audio.tsx
+++ soundgasm-playlist-front/src/components/Audio.tsx
@@ -1,25 +1,26 @@
+import IAudio from "../scripts/IAudio"
+import { Trash } from "../assets/Media/MediaHelper";
 import { useAudioListContext } from "../contexts/AudioListProvider";
 import { useCurrentAudioContext } from "../contexts/CurrentAudioProvider";
-import IAudio from "../scripts/IAudio"
 
 export default function Audio({ data, index } : { data: IAudio, index: number }) {
-    const { audioList, setAudioList } = useAudioListContext();
     const { setCurrIndex } = useCurrentAudioContext();
+    const { audioList, setAudioList } = useAudioListContext();
+    
     let { title } = data
     title.length > 62 ? title = title.slice(0, 61) + "..." : null
 
     function handleClick() { setCurrIndex(index) }
-    function handleDeletion() {
-        const newList = audioList.filter((_, i) => i != index)
-        setAudioList(newList);
-    }
+    function handleDeletion() { setAudioList(audioList.filter((_, i) => i != index)); }
 
     return (
         <div className="flex items-center w-[568px] h-[32px] bg-[#0f1114] rounded-lg relative">
             <button className="h-max w-max rounded-lg" onClick={handleClick}>
                 <span className="text-center m-2">{title}</span>
             </button>
-            <button className="absolute right-3" onClick={handleDeletion}>delete</button>
+            <button className="absolute right-3" onClick={handleDeletion}>
+                <img src={Trash} alt="delete audio" />
+            </button>
         </div>
     )
 }
