36e8135d3443e780dbee033522150cccfc54106c
Ruan <ruan.oliveiralopes@hotmail.com>
2023-12-19 07:12:24 -0300

    minor thing
    - changed variable name

--- soundgasm-playlist-front/src/scripts/GetAudioInfo.ts
+++ soundgasm-playlist-front/src/scripts/GetAudioInfo.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export async function GetAudioInfo(url: string) {
-    const dete = (await axios.post("http://localhost:8080/", { link: `${url}` })).data;
-    return dete;
+    const audio = (await axios.post("http://localhost:8080/", { link: `${url}` })).data;
+    return audio;
 }
