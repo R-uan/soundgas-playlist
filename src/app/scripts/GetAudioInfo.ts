05062ec27e0379f2867788d17fa0ce1bf08b530d
Ruan <ruan.oliveiralopes@hotmail.com>
2024-01-19 21:41:17 -0300

    .env dev

--- src/app/scripts/GetAudioInfo.ts
+++ src/app/scripts/GetAudioInfo.ts
@@ -1,10 +1,10 @@
 import axios, { isAxiosError } from "axios";
 const api = "https://audio-playlist-ru.netlify.app/api";
-const localhost = "http://localhost:3000/api";
+const BackEndURL = process.env.NEXT_PUBLIC_BACKEND_URL || "http://localhost:3000/api";
 
 export async function GetAudioInfo(url: string) {
 	try {
-		const request = await axios.post(api, {
+		const request = await axios.post(BackEndURL, {
 			link: `${url}`,
 		});
 		const audioData = request.data;
